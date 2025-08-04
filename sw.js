4ec20dae45fe448608b12dcee2f3c20ca45957b8
Makibul <142709442+devMakibul@users.noreply.github.com>
2025-08-04 09:48:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-// sw.js
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  clients.claim();
-});
-
-// Optional: cache important assets (not strictly required for share target)
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('fetch', () => {});
